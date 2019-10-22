9d605a1458a57d0b0fbf2e952c02ba12f1443cef
acrush37 <acrush37@gmail.com>
2019-10-22 17:36:47 +0800

    LeetCode Dynamic Programming

--- src/medium/IntegerBreak.java
+++ src/medium/IntegerBreak.java
@@ -1,6 +1,28 @@
 package medium;
 
+/*
+    Given a positive integer n, break it into the sum of at least two positive integers
+    and maximize the product of those integers. Return the maximum product you can get.
+ */
+
 public class IntegerBreak {
 
+    public static void main(String... args) {
+
+        IntegerBreak integerBreak = new IntegerBreak();
+        System.out.println(integerBreak.integerBreak(10));
+    }
+
+    public int integerBreak(int n) {
+
+        int[] f = new int[n+1];
+        f[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            for (int j = 1; j < i; j++)
+                f[i] = Math.max(f[i], Math.max(j * (i-j), f[j] * (i-j)));
+
+        return f[n];
+    }
 
 }
--- src/medium/LongestCommonSubsequence.java
+++ src/medium/LongestCommonSubsequence.java
@@ -1,6 +1,39 @@
 package medium;
 
+/*
+    Given two strings text1 and text2, return the length of their longest common subsequence.
+
+    A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters.
+    (eg, "ace" is a subsequence of "abcde" while "aec" is not).
+    A common subsequence of two strings is a subsequence that is common to both strings.
+
+    If there is no common subsequence, return 0.
+ */
+
 public class LongestCommonSubsequence {
 
+    public static void main(String... args) {
+
+        LongestCommonSubsequence longestCommonSubsequence = new LongestCommonSubsequence();
+        System.out.println(longestCommonSubsequence.longestCommonSubsequence("abcde", "ace"));
+        System.out.println(longestCommonSubsequence.longestCommonSubsequence("abc", "abc"));
+        System.out.println(longestCommonSubsequence.longestCommonSubsequence("abc", "def"));
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        int m = text1.length();
+        int n = text2.length();
+        int[][] f = new int[m+1][n+1];
+        char[] a = text1.toCharArray();
+        char[] b = text2.toCharArray();
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (a[i-1] == b[j-1]) f[i][j] = f[i-1][j-1] + 1;
+                else f[i][j] = Math.max(f[i-1][j], f[i][j-1]);
+
+        return f[m][n];
+    }
 
 }
