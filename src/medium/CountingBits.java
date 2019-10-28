158cbf4a0c8df9fe4c945f2abd172afa4263216c
acrush37 <acrush37@gmail.com>
2019-10-29 03:04:36 +0800

    LeetCode Dynamic Programming

--- src/medium/CountingBits.java
+++ src/medium/CountingBits.java
@@ -1,6 +1,38 @@
 package medium;
 
+/*
+    Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num
+    calculate the number of 1's in their binary representation and return them as an array.
+ */
+
 public class CountingBits {
 
+    public static void main(String... args) {
+
+        CountingBits countingBits = new CountingBits();
+        System.out.println(countingBits.countBits(2));
+        System.out.println(countingBits.countBits(5));
+    }
+
+    public int[] countBits(int num) {
+
+        int[] f = new int[num+1];
+
+        for (int i = 1; i <= num; i++) {
+
+            int x = i;
+            int s = 0;
+
+            while (x != 0) {
+
+                s += (x & 1);
+                x >>= 1;
+            }
+
+            f[i] = s;
+        }
+
+        return f;
+    }
 
 }
