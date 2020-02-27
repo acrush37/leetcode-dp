f6523688b260f5e01962ff9dcde675fc77b00a68
acrush37 <acrush37@gmail.com>
2020-02-27 14:21:40 +0800

    LeetCode Dynamic Programming

--- src/hard/CountDifferentPalindromicSubsequences.java
+++ src/hard/CountDifferentPalindromicSubsequences.java
@@ -13,11 +13,44 @@ public class CountDifferentPalindromicSubsequences {
 
     public int countPalindromicSubsequences(String S) {
 
-        int result = 0;
+        int n = S.length(), p = 1000000007;
+        char[] c = S.toCharArray();
+        int[] a = {-1, -1, -1, -1};
+        int[][] f = new int[n][n];
+        int[] l = new int[n], r = new int[n];
 
-        
+        for (int i = 0; i < n; i++) {
 
-        return result;
+            l[i] = n;
+            r[i] = -1;
+            f[i][i] = 1;
+            int x = c[i] - 97;
+
+            if (a[x] != -1) {
+                l[a[x]] = i;
+                r[i] = a[x];
+            }
+
+            a[x] = i;
+        }
+
+        for (int i = n-2; i >= 0; i--)
+            for (int j = i+1; j < n; j++) {
+
+                if (c[i] != c[j]) f[i][j] = f[i][j-1] + f[i+1][j] - f[i+1][j-1];
+                else {
+
+                    int x = Math.min(l[i], j), y = Math.max(r[j], x-1);
+
+                    if (x > y) f[i][j] = (f[i+1][j-1] << 1) + 2;
+                    else if (x == y) f[i][j] = (f[i+1][j-1] << 1) + 1;
+                    else f[i][j] = (f[i+1][j-1] << 1) - f[x+1][y-1];
+                }
+
+                f[i][j] = (f[i][j] % p + p) % p;
+            }
+
+        return f[0][n-1];
     }
 
 }
